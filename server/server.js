03a193cb5c42942fb422c7cb097ce745682dda26
unknown <C:\Program Files (x86)\The Bat!\Mail>
2017-05-15 17:31:54 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var { mongoose } = require('./db/mongoose');
+const { ObjectID } = require('mongodb');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
@@ -25,10 +26,28 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({ todos });
+        res.send({
+            todos
+        });
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+
+// GET /todos/1234
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }, (e) => res.status(400).send());
 });
 
 app.listen(3000, () => {
@@ -36,3 +55,4 @@ app.listen(3000, () => {
 });
 
 module.exports = { app };
+
